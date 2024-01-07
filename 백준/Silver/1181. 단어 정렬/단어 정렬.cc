716336e992af0e67f1d34f0f9407c3e6dcf476f1
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-01-07 17:05:35 +0900

    [Silver V] Title: 단어 정렬, Time: 1236 ms, Memory: 4528 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -19,6 +19,7 @@ int main()
 	{
 		cin >> str;
 
+		// 벡터 안에 이미 있는 것인지 algorithm.find로 확인.
 		if (find(name.begin(), name.end(), str) == name.end())
 		{
 			name.push_back(str);
@@ -26,11 +27,11 @@ int main()
 	}
 
 	// 벡터 안을 정렬
-
 	sort(name.begin(), name.end(), [](const string& ls, const string& rs) {
+		// 길이가 같으면 사전식 정렬.
 		if (ls.length() == rs.length())
 			return ls < rs;
-		else return ls.length() < rs.length();
+		else return ls.length() < rs.length(); // 그렇지 않으면 길이가 짧은 것 먼저 나오게.
 		});
 
 	for (int i = 0; i < name.size(); i++)
