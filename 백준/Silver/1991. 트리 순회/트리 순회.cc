410eae8c48811e78b5d5fc8f722653d7d69f13e1
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-01-28 20:43:10 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+pair<char, char> edge[26];
+
+// 중위 순회
+void inorder(char root) {
+	if (root == '.') return;
+	inorder(edge[root - 'A'].first);
+	cout << root;
+	inorder(edge[root - 'A'].second);
+}
+
+// 전위 순회
+void preorder(char root) {
+	if (root == '.') return;
+	cout << root;
+	preorder(edge[root - 'A'].first);
+	preorder(edge[root - 'A'].second);
+}
+
+// 후위 순회
+void backorder(char root) {
+	if (root == '.') return;	
+	backorder(edge[root - 'A'].first);
+	backorder(edge[root - 'A'].second);
+	cout << root;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	char root, child_left, child_right;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> root >> child_left >> child_right;
+		edge[root - 'A'] = pair<char, char>(child_left, child_right);
+	}		
+
+	preorder('A');
+	cout << "\n";
+
+	inorder('A');
+	cout << "\n";
+
+	backorder('A');	
+}
