ab1583daf1e645d4a2c4e82d3ce8381022d50d6f
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-02-13 16:36:26 +0900

    [level 2] Title: 전화번호 목록, Time: 69.08 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -7,28 +7,23 @@ bool solution(vector<string> phone_book) {
 
     sort(phone_book.begin(), phone_book.end());
 
-    for (int i = 0; i < phone_book.size(); i++)
+    for (int i = 0; i < phone_book.size() - 1; i++)
     {
-        for (int j = i + 1; j < phone_book.size(); j++)
+        int minLen = min(phone_book[i].length(), phone_book[i+1].length());
+
+        bool check = true;
+
+        for (int k = 0; k < minLen; k++)
         {
-            int minLen = min(phone_book[i].length(), phone_book[j].length());
-
-            bool check = true;
-
-            for (int k = 0; k < minLen; k++)
+            if (phone_book[i].at(k) != phone_book[i+1].at(k))
             {
-                if (phone_book[i].at(k) != phone_book[j].at(k))
-                {
-                    check = false;
-                    break;
-                }
-            }
-
-            if (check)
-                return false;
-            else
+                check = false;
                 break;
+            }
         }
+
+        if (check)
+            return false;
     }
     
     return true;
