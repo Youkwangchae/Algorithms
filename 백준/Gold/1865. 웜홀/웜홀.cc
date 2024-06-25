189a006cce6f7935450a967a61320370f3169cf1
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-06-25 19:22:41 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2348 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,70 +1,90 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
-#define INF 1e9
-
 using namespace std;
-int N, E, W;
-vector<int> edge[3];
-long long dist[510] = { 0, };
 
-void bellman(int start) {
-	for (int i = 1; i <= N; i++) dist[i] = INF;
-	memset(dist, -1, sizeof(dist));
+int n, m, w;
 
-	dist[start] = 0;
+class Edge
+{
+public:
+	int a, b, w;
+};
 
-	for (int i = 1; i <= N - 1; i++) {
-		for (int j = 0; j < edge[0].size(); j++) {
-			int cur = edge[0][j];
-			int to = edge[1][j];
-			int cost = edge[2][j];
-			if (dist[cur] == INF) continue;
-			if (dist[cur] + cost < dist[to]) {
-				dist[to] = dist[cur] + cost;
+bool bellman_ford(int start, vector<int> &dist, vector<Edge> & Edges)
+{
+	for (int i = 1; i < n; i++)
+	{
+		for (int j=0;j<Edges.size();j++)
+		{
+			int begin = Edges[j].a;
+			int end = Edges[j].b;
+			int cost = Edges[j].w;
+
+			if (dist[end] > dist[begin] + cost)
+			{
+				dist[end] = dist[begin] + cost;				
 			}
 		}
-	}
-	for (int i = 0; i < edge[0].size(); i++) {
-		int cur = edge[0][i];
-		int to = edge[1][i];
-		int cost = edge[2][i];
-		if (dist[cur] == INF) continue;
-		if (dist[cur] + cost < dist[to]) {
-			cout << "YES\n";
-			return;
+	}	
+
+	for (int j = 0; j < Edges.size(); j++)
+	{
+		int begin = Edges[j].a;
+		int end = Edges[j].b;
+		int cost = Edges[j].w;
+
+		if (dist[end] > dist[begin] + cost)
+		{			
+			return true;
 		}
 	}
-	cout << "NO\n";
-	return;
+
+	return false;
+}
+
+void solve()
+{
+	cin >> n >> m >> w;
+
+	vector<Edge> Edges;
+
+	vector<int> dist;
+	dist.resize(n + m, 1e9);
+
+	int a, b, c;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> a >> b >> c;
+		Edges.push_back({ a, b, c });		
+		Edges.push_back({ b, a, c });
+	}
+
+	for (int i = 1; i <= w; i++)
+	{
+		cin >> a >> b >> c;
+		Edges.push_back({ a, b, -c });
+	}
+
+	if (bellman_ford(n, dist, Edges))
+	{
+		cout << "YES" << '\n';		
+	}
+	else
+		cout << "NO" << '\n';
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int tc;
 	cin >> tc;
-	while (tc--) {
-		cin >> N >> E >> W;
-		int str, end, val;
-		for (int i = 0; i < E; i++) {
-			cin >> str >> end >> val;
-			edge[0].push_back(str); edge[0].push_back(end);
-			edge[1].push_back(end);	edge[1].push_back(str);
-			edge[2].push_back(val); edge[2].push_back(val);
-		}
-		for (int i = 0; i < W; i++) {
-			cin >> str >> end >> val;
-			edge[0].push_back(str);
-			edge[1].push_back(end);
-			edge[2].push_back(-val);
-		}
 
-		bellman(1);
-		edge[0].clear();
-		edge[1].clear();
-		edge[2].clear();
+	for (int k = 1; k <= tc; k++)
+	{
+		solve();
 	}
 }
