3f0d29ca79bf2fa101aef6bfe40c92583f38ec87
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-06-25 19:43:15 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -7,34 +7,32 @@ int n, m, w;
 class Edge
 {
 public:
-	int a, b, w;
+	int a, b;
+	int w = 1e9;
 };
 
-bool bellman_ford(int start, vector<int> &dist, vector<Edge> & Edges)
-{
-	for (int i = 1; i < n; i++)
+vector<Edge> Edges;
+
+bool bellman_ford(int start)
+{	
+	vector<int> dist;
+	dist.resize(n + m, 0);								// 모든 점에서 벨만-포드를 돌리기 위해 모든 거리를 INF 대신 0으로 초기화.
+
+	for (int i = 0; i < n; i++)
 	{
-		for (int j=0;j<Edges.size();j++)
-		{
-			int begin = Edges[j].a;
-			int end = Edges[j].b;
-			int cost = Edges[j].w;
-
-			if (dist[end] > dist[begin] + cost)
-			{
-				dist[end] = dist[begin] + cost;				
-			}
-		}
-	}	
-
-	for (int j = 0; j < Edges.size(); j++)
-	{
-		int begin = Edges[j].a;
-		int end = Edges[j].b;
-		int cost = Edges[j].w;
-
-		if (dist[end] > dist[begin] + cost)
+		for (const auto& e : Edges)
 		{			
+			if (dist[e.a] == 1e9) continue;
+			dist[e.b] = min(dist[e.b], dist[e.a] + e.w);
+		}
+	}
+
+	for (const auto& e : Edges)
+	{
+		if (dist[e.a] == 1e9) continue;
+
+		if (dist[e.b] > dist[e.a] + e.w)
+		{
 			return true;
 		}
 	}
@@ -42,21 +40,42 @@ bool bellman_ford(int start, vector<int> &dist, vector<Edge> & Edges)
 	return false;
 }
 
+//bool bellman_ford(int start)
+//{
+//	vector<int> dist;
+//	dist.resize(n + m, 1e9);								
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (const auto& e : Edges)
+//		{												// 모든 점이 INF인지 확인하지 않으므로, 모든 점에서 벨만-포드 구할 수 있음.
+//			dist[e.b] = min(dist[e.b], dist[e.a] + e.w);
+//		}
+//	}
+//
+//	for (const auto& e : Edges)
+//	{//													// 모든 점이 INF인지 확인하지 않으므로, 모든 점에서 벨만-포드 구할 수 있음.
+//		if (dist[e.b] > dist[e.a] + e.w)
+//		{
+//			return true;
+//		}
+//	}
+//
+//	return false;
+//}
+
 void solve()
 {
 	cin >> n >> m >> w;
 
-	vector<Edge> Edges;
-
-	vector<int> dist;
-	dist.resize(n + m, 1e9);
+	Edges.clear();	
 
 	int a, b, c;
 
 	for (int i = 1; i <= m; i++)
 	{
 		cin >> a >> b >> c;
-		Edges.push_back({ a, b, c });		
+		Edges.push_back({ a, b, c });
 		Edges.push_back({ b, a, c });
 	}
 
@@ -64,12 +83,10 @@ void solve()
 	{
 		cin >> a >> b >> c;
 		Edges.push_back({ a, b, -c });
-	}
+	}		
 
-	if (bellman_ford(n, dist, Edges))
-	{
-		cout << "YES" << '\n';		
-	}
+	if (bellman_ford(1))	
+		cout << "YES" << '\n';			
 	else
 		cout << "NO" << '\n';
 }
@@ -77,8 +94,8 @@ void solve()
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(0);
+	cout.tie(0);
 
 	int tc;
 	cin >> tc;
