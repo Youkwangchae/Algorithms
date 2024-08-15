b462fa6162843f861e3fb692e57def0c3e12ae3a
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-08-15 23:45:16 +0900

    [Silver III] Title: N과 M (7), Time: 220 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9];
+bool Visit[10000];
+
+void solve(string s, int count)
+{
+	if (count == m)
+	{
+		cout << s << '\n';		
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{			
+			solve(s + to_string(arr[i]) + " ", count + 1);
+		}	
+	}
+	return;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;	
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+
+	solve("", 0);
+}
